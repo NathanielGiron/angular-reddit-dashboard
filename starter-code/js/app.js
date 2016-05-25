b912dc9007c62dca07e1e2308d2eef1da859d5b3
Nathaniel Giron <nathaniel.giron@gmail.com>
2016-05-25 09:11:04 -0700

    initial submission

--- starter-code/js/app.js
+++ starter-code/js/app.js
@@ -1,3 +1,27 @@
 /* put js code here */
+var app = angular.module('RedditApp', []);
+
+app.controller('SearchCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.searchTerm = '';
+	$scope.history = [];
+
+	$scope.search = function() {
+		$scope.history.push($scope.searchTerm);
+		var req = {
+			url: "http://www.reddit.com/search.json?q=" + $scope.searchTerm,
+			method: "GET"
+		}
+
+		$http(req).then(function success(res) {
+			var redditData = res.data;
+			console.log(redditData);
+			$scope.results = redditData.data.children;
+		}, function error(res) {
+			console.log(res);
+		});
+	}
+
+
+}]);
 
 console.log('this file is running.');
